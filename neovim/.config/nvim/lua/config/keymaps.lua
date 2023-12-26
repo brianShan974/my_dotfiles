fb1f4d378dfd81f324506cba8e339cde2bac0830
brianShan974 <brianshan974@outlook.com>
2023-12-26 16:32:52 -0500

    Added new keymaps, inspired by AstroNvim. Added a few plugins.

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -3,33 +3,33 @@
 local util = require("util")
 
 local function map(mode, lhs, rhs, opts)
-	local keys = require("lazy.core.handler").handlers.keys
-	---@cast keys LazyKeysHandler
-	-- do not create the keymap if a lazy keys handler exists
-	if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-		opts = opts or {}
-		opts.silent = opts.silent ~= false
-		vim.keymap.set(mode, lhs, rhs, opts)
-	end
+    local keys = require("lazy.core.handler").handlers.keys
+    ---@cast keys LazyKeysHandler
+    -- do not create the keymap if a lazy keys handler exists
+    if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+        opts = opts or {}
+        opts.silent = opts.silent ~= false
+        vim.keymap.set(mode, lhs, rhs, opts)
+    end
 end
 
 -- Paste over currently selected text without yanking it
 map("v", "p", '"_dP', { silent = true })
 
 -- go to files
-map("n", "<leader>go", "<cmd>e ~/.config/fvim/lua/config/options.lua<cr>", { desc = "Go to options config" })
-map("n", "<leader>gk", "<cmd>e ~/.config/fvim/lua/config/keymaps.lua<cr>", { desc = "Go to keymaps config" })
+map("n", "<leader>go", "<cmd>e ~/.config/neovim/lua/config/options.lua<cr>", { desc = "Go to options config" })
+map("n", "<leader>gk", "<cmd>e ~/.config/neovim/lua/config/keymaps.lua<cr>", { desc = "Go to keymaps config" })
 map("n", "<leader>gs", function()
-	require("luasnip.loaders").edit_snippet_files({})
+    require("luasnip.loaders").edit_snippet_files({})
 end, { desc = "Go to luasnip config" })
 map("n", "<leader>gp", function()
-  require("neo-tree.command").execute({ toggle = true, dir = "/Users/hanyu_yan/.config/fvim/lua/plugins" })
+    require("neo-tree.command").execute({ toggle = true, dir = "~/.config/neovim/lua/plugins" })
 end, { desc = "Go to plugins config" })
 map(
-	"n",
-	"<leader>gl",
-	"<cmd>Telescope file_browser path=~/.config/fvim/lua/plugins<cr>",
-	{ desc = "Go to lazyvim config" }
+    "n",
+    "<leader>gl",
+    "<cmd>Telescope file_browser path=~/.config/neovim/lua/plugins<cr>",
+    { desc = "Go to lazyvim config" }
 )
 
 -- better movement
@@ -103,17 +103,42 @@ map("n", ">", "v>g")
 -- map("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Toggle format on Save" })
 -- ===
 map("n", "<leader>us", function()
-	util.toggle("spell")
+    util.toggle("spell")
 end, { desc = "Toggle Spelling" })
 map("n", "<leader>uw", function()
-	util.toggle("wrap")
+    util.toggle("wrap")
 end, { desc = "Toggle Word Wrap" })
 map("n", "<leader>ud", util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
 local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
 map("n", "<leader>uc", function()
-	util.toggle("conceallevel", false, { 0, conceallevel })
+    util.toggle("conceallevel", false, { 0, conceallevel })
 end, { desc = "Toggle Conceal" })
 
 -- Others
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+map("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
 map("n", "<leader>L", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+
+-- Go to home screen, copied from AstroNvim
+vim.keymap.set(
+    "n",
+    "<leader>h",
+    function()
+        local wins = vim.api.nvim_tabpage_list_wins(0)
+        if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
+            vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+        end
+        require("alpha").start(false, require("alpha").default_config)
+    end
+)
+
+-- Toggle comment, copied from AstroNvim
+vim.keymap.set("n", "<C-/>",
+    function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end
+)
+vim.keymap.set("v", "<C-/>",
+    "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>"
+)
+
+-- Toggle terminal
+vim.keymap.set("n", "<F7>", "<cmd>ToggleTerm direction=float<cr>")
+vim.keymap.set("n", "<F8>", "<cmd>ToggleTerm direction=horizontal<cr>")
